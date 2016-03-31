4f6365eadb429d5beb283ab506958b70558e204a
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-03-31 01:10:02 -0400

    added tableview and segue

--- CoinTossWithHistory/HistoryViewController.swift
+++ CoinTossWithHistory/HistoryViewController.swift
@@ -21,15 +21,7 @@ class HistoryViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func dismiss(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
-    */
-
 }
